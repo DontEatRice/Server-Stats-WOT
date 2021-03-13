1c8eb8cf2828a416348ea0175d3ab34387f7e72a
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2021-03-14 00:33:05 +0100

    fixed client-js bugs, new routing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,44 @@
 stats.reverse();
 const colors = ["rgba(255,99,132,1)", "rgba(103, 181, 190, 1)"]
 
-let labels = []
-let players = []
-let names = []
-let players_sum = []
-for(let i = 0; i<stats[0].servers.length; i++) {
-  players.push([])
-}
-stats[0].servers.forEach(server => {
-  names.push(server.name)
-})
-
-stats.forEach((value, index) => {
-  let date = new Date(value.date);
-  if (date.getHours() === 23)
-    labels.push(`${date.getDate()}.${date.getMonth() < 9 ? '0'+(date.getMonth()+1): date.getMonth()+1} ${date.getHours() < 10 ? '0'+date.getHours(): date.getHours()}`);
-  else
-    labels.push(`${date.getHours() < 10 ? '0'+date.getHours(): date.getHours()}`)
-  let sum = 0;
-  value.servers.forEach((server, index) => {
-    sum += server.players
-    players[index].push(server.players)
+const setExampleData = (stats) => {
+  let structure = []
+  stats[0].servers.forEach(server => {
+    structure.push(server.name)
   })
-  players_sum.push(sum)
-});
+  stats.forEach(stat => {
+    if(stat.servers.length > structure.length){
+      structure = []
+      stat.servers.forEach(server => {
+        structure.push(server.name)
+      })
+    }
+  })
+  return structure
+} 
+
+const makeValid = (stats, example) => {
+  stats.forEach(stat => {
+    if (stat.servers.length !== example.length) {
+      for(let i = 0; i < example.length; i++) {
+        if (stat.servers[i].name != example[i]) {
+          stat.servers.splice(i, 0, {players: NaN, name: example[i]})
+        }
+      }
+    }
+  })
+}
+const randomInt = (n) => {
+  return Math.floor(Math.random()*n)
+}
+
+const randomColor = () => {
+  const r = randomInt(256)
+  const g = randomInt(256)
+  const b = randomInt(256)
+
+  return `rgba(${r}, ${g}, ${b}, 1)`
+}
 
 const datasetsGenerator = (name, data, color) => {
   return {
@@ -42,39 +56,41 @@ const datasetsGenerator = (name, data, color) => {
   }
 }
 
+
+let labels = []
+let players = []
+let players_sum = []
+const example = setExampleData(stats)
+makeValid(stats, example)
+for(let i = 0; i<example.length; i++) {
+  players.push([])
+}
+
+stats.forEach((value, index) => {
+  let date = new Date(value.date);
+  if (date.getHours() === 23)
+    labels.push(`${date.getDate()}.${date.getMonth() < 9 ? '0'+(date.getMonth()+1): date.getMonth()+1} ${date.getHours() < 10 ? '0'+date.getHours(): date.getHours()}`);
+  else
+    labels.push(`${date.getHours() < 10 ? '0'+date.getHours(): date.getHours()}`)
+  let sum = 0;
+  value.servers.forEach((server, index) => {
+    sum += isNaN(server.players) ? 0 : server.players;
+    players[index].push(server.players)
+  })
+  players_sum.push(sum)
+});
+
+
+
 let datasets = []
-names.forEach((name, index)=> {
-  datasets.push(datasetsGenerator(name, players[index], colors[index]))
+example.forEach((name, index)=> {
+  datasets.push(datasetsGenerator(name, players[index], index >= colors.length ? randomColor() : colors[index]))
 })
 datasets.push(datasetsGenerator('sum', players_sum, "rgba(236, 71, 233, 1)"))
 
 
 const data = {
     labels: labels,
-    // datasets: [{
-    //   label: "EU1",
-    //   borderColor: "rgba(255,99,132,1)",
-    //   fill: false,
-    //   backgroundColor: "rgba(255,99,132,1)",
-    //   borderWidth: 3,
-    //   hoverBackgroundColor: "rgba(255,99,132,1)",
-    //   hoverBorderColor: "rgba(255,99,132,1)",
-    //   data: EU1Stats,
-    //   pointRadius: 3,
-    //   pointHoverRadius: 6,
-    // },
-    // {
-    //   label: "EU2",
-    //   borderColor: "rgba(103, 181, 190, 1)",
-    //   borderWidth: 3,
-    //   hoverBackgroundColor: "rgba(103, 181, 190, 1)",
-    //   hoverBorderColor: "rgba(103, 181, 190, 1)",
-    //   data: EU2Stats,
-    //   pointRadius: 3,
-    //   fill: false,
-    //   backgroundColor: "rgba(103, 181, 190, 1)",
-    //   pointHoverRadius: 6,
-    // }]
     datasets: datasets
 };
   
@@ -121,3 +137,9 @@ const options = {
     options: options,
     data: data
   });
+
+
+const select = document.querySelector('select')
+select.addEventListener('change', () => {
+  window.location.href = window.location.origin + "/" + select.value.toLowerCase();
+})
--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,42 @@
 var express = require('express');
 var router = express.Router();
-const Stats = require('../models/stats').EU
+const Stats = require('../models/stats')
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  const findStats = Stats.find().sort({date: -1}).limit(25).select("servers date")//{date: {"$lte": drugi.toISOString()}}
+const getData = (res, db, loc, mode) => {
+  const findStats = db.find().sort({date: -1}).limit(25).select("servers date")//{date: {"$lte": drugi.toISOString()}}
   findStats.exec((err, data) => {
     if (err)
       res.render('error', {message: err});
     else 
-      res.render('main', { data: data});
+      res.render('main', { data: data, loc: loc, mode: mode});
   })
+}
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.redirect('/eu');
 });
 
-router.get('/api', (req, res) => {
-  // const drugi = new Date();
-  const findStats = Stats.find()//{date: {"$lte": drugi.toISOString()}}
-  findStats.exec((err, data) => {
-    if (err)
-      res.send(err);
-    else {
-      // data.forEach((value) => {
-      //   Stats.findByIdAndRemove(value.id, err => {
-      //     if (err)
-      //       console.log(err)
-      //     else
-      //       console.log('Sukces')
-      //   })
-      // });
-      // console.log('koniec')
-      res.send(data)
-    } 
-      
-  })
-  
+router.get('/eu', (req, res) => {
+  getData(res, Stats.EU, 'EU');
 })
+
+router.get('/na', (req, res) => {
+  getData(res, Stats.NA, 'NA');
+});
+
+router.get('/ru', (req, res) => {
+  getData(res, Stats.RU, 'RU');
+});
+
+router.get('/asia', (req, res) => {
+  getData(res, Stats.ASIA, 'ASIA');
+});
+
+router.get('/week/:serv', (req, res) => {
+  res.send(req.params.serv);
+})
+
 
 module.exports = router;
